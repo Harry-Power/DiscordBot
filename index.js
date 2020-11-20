287054f8e4b7b990cebc3f5830d52a6b60fffa6c
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 17:46:09 +0000

    Added ability to change some config items with messages in chat

--- index.js
+++ index.js
@@ -163,6 +163,26 @@ client.on('message', message => {
       message.reply('Please use <#' + message.guild.channels.cache.find(ch => ch.name === config.botchannel) + '>')
     };
   }
+  else if (command == "config") {
+    if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply('You need to be an admin to use that command')
+    else if (args[0] == "welcomemsg" ) {
+      message.channel.send("Welcome message :  " + config.welcomemsg + "   =>   " + args[1])
+      config.welcomemsg = args[1]
+    }
+    else if (args[0] == "prefix" ) {
+      message.channel.send("Prefix :  " + config.prefix + "   =>   " + args[1])
+      config.prefix = args[1]
+    }
+    else if (args[0] == "botchannel" ) {
+      message.channel.send("Bot channel name :  " + config.botchannel + "   =>   " + args[1])
+      config.botchannel = args[1]
+    }
+    else if (args[0] == "prefix" ) {
+      message.channel.send("Username suggestion channel :  " + config.usernamech + "   =>   " + args[1])
+      config.usernamech = args[1]
+    }
+
+  }
 })
 
 client.login(config.token)
